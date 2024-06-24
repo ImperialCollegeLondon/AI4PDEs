83ad2d673aeedd37b41098fd517d7c9052ca1d66
Boyang Chen <95478327+bc1chen@users.noreply.github.com>
2024-06-24 22:23:06 +0100

    update the new AI4PDEs modules

--- ai4pdes/structured/AI4PDEs_bounds.py
+++ ai4pdes/structured/AI4PDEs_bounds.py
@@ -1,96 +1,39 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2024 
+#
+# Christopher C. Pain   c.pain@imperial.ac.uk
+# Claire E. Heaney      c.heaney@imperial.ac.uk
+# Boyang Chen           boyang.chen16@imperial.ac.uk
+#
+# Applied Modelling and Computation Group
+# Department of Earth Science and Engineering
+# Imperial College London
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+#-- Import general libraries
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def boundary_condition_Dirichlet_2D(values, values_pad, face):
-	'''
-	face
-	0 - west 0
-	1 - east nx
-	2 - top  0
-	3 - bot  ny
-	'''
-	ny = values.shape[2]
-	nx = values.shape[3]
-	nny = values_pad.shape[2]
-	nnx = values_pad.shape[3]
-	if face == 0:
-		values_pad[0,0,:,0].fill_(0.0)
-	elif face == 1:
-		values_pad[0,0,:,nx+1].fill_(0.0)
-	elif face == 2:
-		values_pad[0,0,0,:].fill_(0.0)
-	elif face == 3:
-		values_pad[0,0,ny+1,:].fill_(0.0)
-	return values, values_pad
-
-def boundary_condition_Neumann_2D(values, values_pad, face):
-	'''
-	face
-	0 - west 0
-	1 - east nx
-	2 - top  0
-	3 - bot  ny
-	'''
-	ny = values.shape[2]
-	nx = values.shape[3]
-	nny = values_pad.shape[2]
-	nnx = values_pad.shape[3]
-	if face == 0:
-		values_pad[0,0,:,0] = values_pad[0,0,:,1]
-	elif face == 1:
-		values_pad[0,0,:,nx+1] = values_pad[0,0,:,nx]
-	elif face == 2:
-		values_pad[0,0,0,:] = values_pad[0,0,1,:]
-	elif face == 3:
-		values_pad[0,0,ny+1,:] = values_pad[0,0,ny,:]
-	return values, values_pad
-
-def boundary_condition_inflow_2D(values, values_pad, face, ub):
-	'''
-	face
-	0 - west 0
-	1 - east nx
-	2 - top  0
-	3 - bot  ny
-	'''
-	ny = values.shape[2]
-	nx = values.shape[3]
-	nny = values_pad.shape[2]
-	nnx = values_pad.shape[3]
-	if face == 0:
-		values_pad[0,0,:,0].fill_(ub)
-	elif face == 1:
-		values_pad[0,0,:,nx+1].fill_(ub)
-	elif face == 2:
-		values_pad[0,0,0,:].fill_(ub)
-	elif face == 3:
-		values_pad[0,0,ny+1,:].fill_(ub)
-	return values, values_pad
-
-def boundary_condition_outflow_2D(values, values_pad, face, ub):
-	'''
-	face
-	0 - west 0
-	1 - east nx
-	2 - top  0
-	3 - bot  ny
-	'''
-	ny = values.shape[2]
-	nx = values.shape[3]
-	nny = values_pad.shape[2]
-	nnx = values_pad.shape[3]
-	if face == 0:
-		values_pad[0,0,:,0].fill_(ub)
-	elif face == 1:
-		values_pad[0,0,:,nx+1].fill_(ub)
-	elif face == 2:
-		values_pad[0,0,0,:].fill_(ub)
-	elif face == 3:
-		values_pad[0,0,ny+1,:].fill_(ub)
-	return values, values_pad
-
-def boundary_condition_u(values_u, values_uu,ub):
+def boundary_condition_2D_u(values_u, values_uu, ub):
 	ny = values_u.shape[2]
 	nx = values_u.shape[3]
 	nny = values_uu.shape[2]
@@ -103,7 +46,7 @@ def boundary_condition_u(values_u, values_uu,ub):
 	values_uu[0,0,ny+1,:].fill_(0.0)
 	return values_uu
     
-def boundary_condition_v(values_v, values_vv,ub):
+def boundary_condition_2D_v(values_v, values_vv, ub):
 	ny = values_v.shape[2]
 	nx = values_v.shape[3]
 	nny = values_vv.shape[2]
@@ -116,7 +59,7 @@ def boundary_condition_v(values_v, values_vv,ub):
 	values_vv[0,0,ny+1,:].fill_(0.0)
 	return values_vv
 
-def boundary_condition_p(values_p, values_pp):
+def boundary_condition_2D_p(values_p, values_pp):
 	ny = values_p.shape[2]
 	nx = values_p.shape[3]
 	nny = values_pp.shape[2]
@@ -129,7 +72,7 @@ def boundary_condition_p(values_p, values_pp):
 	values_pp[0,0,ny+1,:] = values_pp[0,0,ny,:]
 	return values_pp
 
-def boundary_condition_cw(w):
+def boundary_condition_2D_cw(w):
 	ny = w.shape[2]
 	nx = w.shape[3]
 	ww = F.pad(w, (1, 1, 1, 1), mode='constant', value=0)
@@ -137,4 +80,80 @@ def boundary_condition_cw(w):
 	ww[0,0,:,nx+1] = ww[0,0,:,nx]*0
 	ww[0,0,0,:] = ww[0,0,1,:]*0
 	ww[0,0,ny+1,:] = ww[0,0,ny,:]*0
+	return ww
+
+def boundary_condition_3D_u(values_u, values_uu, ub):
+	nz = values_u.shape[2]
+	ny = values_u.shape[3]
+	nx = values_u.shape[4]
+	nnz = values_uu.shape[2]
+	nny = values_uu.shape[3]
+	nnx = values_uu.shape[4]
+
+	values_uu[0,0,1:nnz-1,1:nny-1,1:nnx-1] = values_u[0,0,:,:,:]
+	values_uu[0,0,:,:,0].fill_(ub)
+	values_uu[0,0,:,:,nx+1].fill_(ub)
+	values_uu[0,0,:,0,:] = values_uu[0,0,:,1,:]
+	values_uu[0,0,:,ny+1,:] = values_uu[0,0,:,ny,:]
+	values_uu[0,0,0,:,:].fill_(0.0)
+	values_uu[0,0,nz+1,:,:] = values_uu[0,0,nz,:,:]
+	return values_uu
+    
+def boundary_condition_3D_v(values_v, values_vv, ub):
+	nz = values_v.shape[2]
+	ny = values_v.shape[3]
+	nx = values_v.shape[4]
+	nnz = values_vv.shape[2]
+	nny = values_vv.shape[3]
+	nnx = values_vv.shape[4]
+
+	values_vv[0,0,1:nnz-1,1:nny-1,1:nnx-1] = values_v[0,0,:,:,:]
+	values_vv[0,0,:,:,0].fill_(0.0)
+	values_vv[0,0,:,:,nx+1].fill_(0.0)
+	values_vv[0,0,:,0,:].fill_(0.0)
+	values_vv[0,0,:,ny+1,:].fill_(0.0)
+	values_vv[0,0,0,:,:].fill_(0.0)
+	values_vv[0,0,nz+1,:,:] = values_vv[0,0,nz,:,:]
+	return values_vv
+
+def boundary_condition_3D_w(values_w, values_ww, ub):
+	nz = values_w.shape[2]
+	ny = values_w.shape[3]
+	nx = values_w.shape[4]
+	nnz = values_ww.shape[2]
+	nny = values_ww.shape[3]
+	nnx = values_ww.shape[4]
+
+	values_ww[0,0,1:nnz-1,1:nny-1,1:nnx-1] = values_w[0,0,:,:,:]
+	values_ww[0,0,:,:,0].fill_(0.0)
+	values_ww[0,0,:,:,nx+1].fill_(0.0)
+	values_ww[0,0,:,0,:].fill_(0.0)
+	values_ww[0,0,:,ny+1,:].fill_(0.0)
+	values_ww[0,0,0,:,:].fill_(0.0)
+	values_ww[0,0,nz+1,:,:] = values_ww[0,0,nz,:,:]
+	return values_ww
+
+def boundary_condition_3D_p(values_p, values_pp):
+	nz = values_p.shape[2]
+	ny = values_p.shape[3]
+	nx = values_p.shape[4]
+	nnz = values_pp.shape[2]
+	nny = values_pp.shape[3]
+	nnx = values_pp.shape[4]
+
+	values_pp[0,0,1:nnz-1,1:nny-1,1:nnx-1] = values_p[0,0,:,:,:]
+	values_pp[0,0,:,:,0] =  values_pp[0,0,:,:,1] 
+	values_pp[0,0,:,:,nx+1].fill_(0.0)
+	values_pp[0,0,:,0,:] = values_pp[0,0,:,1,:]
+	values_pp[0,0,:,ny+1,:] = values_pp[0,0,:,ny,:]
+	values_pp[0,0,0,:,:] = values_pp[0,0,1,:,:]
+	values_pp[0,0,nz+1,:,:] = values_pp[0,0,nz,:,:]
+	return values_pp
+
+def boundary_condition_3D_k(k_u):
+	k_uu = F.pad(k_u, (1, 1, 1, 1, 1, 1), mode='constant', value=0)
+	return k_uu
+
+def boundary_condition_3D_cw(w):
+	ww = F.pad(w, (1, 1, 1, 1, 1, 1), mode='constant', value=0)
 	return ww
--- ai4pdes/structured/AI4PDEs_utils.py
+++ ai4pdes/structured/AI4PDEs_utils.py
@@ -1,3 +1,34 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2024 
+#
+# Christopher C. Pain   c.pain@imperial.ac.uk
+# Claire E. Heaney      c.heaney@imperial.ac.uk
+# Boyang Chen           boyang.chen16@imperial.ac.uk
+#
+# Applied Modelling and Computation Group
+# Department of Earth Science and Engineering
+# Imperial College London
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+#-- Import general libraries
 import numpy as np
 import torch
 
@@ -15,6 +46,20 @@ def create_tensors_3D(nx, ny, nz):
     b_uu = torch.zeros(input_shape_pad, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
     b_vv = torch.zeros(input_shape_pad, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
     b_ww = torch.zeros(input_shape_pad, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    print('All the required 3D tensors have been created successfully!')
+    print('===========================================================')
+    print('values_u  => u velocity [first step]  - (1,1,nz,ny,nx)')
+    print('values_v  => v velocity [first step]  - (1,1,nz,ny,nx)')
+    print('values_w  => w velocity [first step]  - (1,1,nz,ny,nx)')
+    print('values_p  => pressure                 - (1,1,nz,ny,nx)')
+    print('b_uu      => v velocity [second step] - (1,1,nz+2,ny+2,nx+2)')
+    print('b_vv      => v velocity [second step] - (1,1,nz+2,ny+2,nx+2)')
+    print('b_ww      => w velocity [second step] - (1,1,nz+2,ny+2,nx+2)')
+    print('values_uu => u velocity [first step]  - (1,1,nz+2,ny+2,nx+2)')
+    print('values_vv => v velocity [first step]  - (1,1,nz+2,ny+2,nx+2)')
+    print('values_ww => w velocity [first step]  - (1,1,nz+2,ny+2,nx+2)')
+    print('values_pp => pressure                 - (1,1,nz+2,ny+2,nx+2)')
+    print('===========================================================')
     return values_u, values_v, values_w, values_p, values_uu, values_vv, values_ww, values_pp, b_uu, b_vv, b_ww
 
 def create_tensors_2D(nx, ny):
@@ -28,7 +73,17 @@ def create_tensors_2D(nx, ny):
     values_pp = torch.zeros(input_shape_pad, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
     b_uu = torch.zeros(input_shape_pad, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
     b_vv = torch.zeros(input_shape_pad, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-    print('All the required tensors have been created successfully!')
+    print('All the required 2D tensors have been created successfully!')
+    print('===========================================================')
+    print('values_u  => u velocity [first step]  - (1,1,ny,nx)')
+    print('values_v  => v velocity [first step]  - (1,1,ny,nx)')
+    print('values_p  => pressure                 - (1,1,ny,nx)')
+    print('b_uu      => v velocity [second step] - (1,1,ny+2,nx+2)')
+    print('b_vv      => v velocity [second step] - (1,1,ny+2,nx+2)')
+    print('values_uu => u velocity [first step]  - (1,1,ny+2,nx+2)')
+    print('values_vv => v velocity [first step]  - (1,1,ny+2,nx+2)')
+    print('values_pp => pressure                 - (1,1,ny+2,nx+2)')
+    print('===========================================================')
     return values_u, values_v, values_p, values_uu, values_vv, values_pp, b_uu, b_vv
 
 def get_weights_linear_2D(dx):
@@ -87,12 +142,100 @@ def get_weights_linear_2D(dx):
     w_res = torch.zeros([1,1,2,2]) 
     w_res[0,0,:,:] = 0.25
     diag = np.array(wA)[0,0,1,1]        # Diagonal component
-    print('All the required filters have been created successfully!')
+    print('All the required 2D filters have been created successfully!')
+    print('===========================================================')
+    print('w1    => second order derivative  - (1,1,3,3)')
+    print('w2    => first order derivative x - (1,1,3,3)')
+    print('w3    => first order derivative y - (1,1,3,3)')
+    print('wA    => second order derivative  - (1,1,3,3)')
+    print('w_res => Restriction operation    - (1,1,3,3)')
+    print('diag  => Diagonal component of wA - (1,1,1,1)')
+    print('===========================================================')
     return w1, w2, w3, wA, w_res, diag
+
+def get_weights_linear_3D(dx):
+    pd1 = torch.tensor([[2/26, 3/26, 2/26],
+           [3/26, 6/26, 3/26],
+           [2/26, 3/26, 2/26]])
+    pd2 = torch.tensor([[3/26, 6/26, 3/26],
+           [6/26, -88/26, 6/26],
+           [3/26, 6/26, 3/26]])
+    pd3 = torch.tensor([[2/26, 3/26, 2/26],
+           [3/26, 6/26, 3/26],
+           [2/26, 3/26, 2/26]])
+    w1 = torch.zeros([1, 1, 3, 3, 3])
+    wA = torch.zeros([1, 1, 3, 3, 3])
+    w1[0, 0, 0,:,:] = pd1/dx**2
+    w1[0, 0, 1,:,:] = pd2/dx**2
+    w1[0, 0, 2,:,:] = pd3/dx**2
+    wA[0, 0, 0,:,:] = -pd1/dx**2
+    wA[0, 0, 1,:,:] = -pd2/dx**2
+    wA[0, 0, 2,:,:] = -pd3/dx**2
+    # Gradient filters
+    p_div_x1 = torch.tensor([[-0.014, 0.0, 0.014],
+           [-0.056, 0.0, 0.056],
+           [-0.014, 0.0, 0.014]])
+    p_div_x2 = torch.tensor([[-0.056, 0.0, 0.056],
+           [-0.22, 0.0, 0.22],
+           [-0.056, 0.0, 0.056]])
+    p_div_x3 = torch.tensor([[-0.014, 0.0, 0.014],
+           [-0.056, 0.0, 0.056],
+           [-0.014, 0.0, 0.014]])
+    p_div_y1 = torch.tensor([[0.014, 0.056, 0.014],
+           [0.0, 0.0, 0.0],
+           [-0.014, -0.056, -0.014]])
+    p_div_y2 = torch.tensor([[0.056, 0.22, 0.056],
+           [0.0, 0.0, 0.0],
+           [-0.056, -0.22, -0.056]])
+    p_div_y3 = torch.tensor([[0.014, 0.056, 0.014],
+           [0.0, 0.0, 0.0],
+           [-0.014, -0.056, -0.014]])
+    p_div_z1 = torch.tensor([[0.014, 0.056, 0.014],
+           [0.056, 0.22, 0.056],
+           [0.014, 0.056, 0.014]])
+    p_div_z2 = torch.tensor([[0.0, 0.0, 0.0],
+           [0.0, 0.0, 0.0],
+           [0.0, 0.0, 0.0]])
+    p_div_z3 = torch.tensor([[-0.014, -0.056, -0.014],
+           [-0.056, -0.22, -0.056],
+           [-0.014, -0.056, -0.014]])
+    w2 = torch.zeros([1,1,3,3,3])
+    w3 = torch.zeros([1,1,3,3,3])
+    w4 = torch.zeros([1,1,3,3,3])
+    w2[0,0,0,:,:] = -p_div_x1/dx*0.5
+    w2[0,0,1,:,:] = -p_div_x2/dx*0.5
+    w2[0,0,2,:,:] = -p_div_x3/dx*0.5
+    w3[0,0,0,:,:] = -p_div_y1/dx*0.5
+    w3[0,0,1,:,:] = -p_div_y2/dx*0.5
+    w3[0,0,2,:,:] = -p_div_y3/dx*0.5
+    w4[0,0,0,:,:] = -p_div_z1/dx*0.5
+    w4[0,0,1,:,:] = -p_div_z2/dx*0.5
+    w4[0,0,2,:,:] = -p_div_z3/dx*0.5
+    # Restriction filters
+    w_res = torch.zeros([1,1,2,2,2])
+    w_res[0,0,:,:,:] = 0.125
+    diag = np.array(wA)[0,0,1,1,1]    # Diagonal component
+    print('All the required 3D filters have been created successfully!')
+    print('===========================================================')
+    print('w1    => second order derivative  - (1,1,3,3,3)')
+    print('w2    => first order derivative x - (1,1,3,3,3)')
+    print('w3    => first order derivative y - (1,1,3,3,3)')
+    print('w4    => first order derivative z - (1,1,3,3,3)')
+    print('wA    => second order derivative  - (1,1,3,3,3)')
+    print('w_res => Restriction operation    - (1,1,3,3,3)')
+    print('diag  => Diagonal component of wA - (1,1,1,1,1)')
+    print('===========================================================')
+    return w1, w2, w3, w4, wA, w_res, diag
 
 def create_solid_body_2D(nx, ny, cor_x, cor_y, size_x, size_y):
     input_shape = (1, 1, ny, nx)
     sigma = torch.zeros(input_shape, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
     sigma[0,0,cor_y-size_y:cor_y+size_y,cor_x-size_x:cor_x+size_x] = 1e08
     print('A bluff body has been created successfully!')
+    print('===========================================')
+    print('Size of body in x:',size_x*2)
+    print('Size of body in y:',size_y*2)
+    print('position of body in x:',cor_x)
+    print('position of body in y:',cor_y)
+    print('===========================================')
     return sigma

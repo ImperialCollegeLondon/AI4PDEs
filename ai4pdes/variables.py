c6ec159d96c52287371e9e5dafbf0e43517df88d
lm2612 <lauraman@stanford.edu>
2024-06-26 17:38:25 +0100

    added temporarily two separate versions of each variable, one for u and one for uu which is padded with halo to keep consistency with old version of code

--- ai4pdes/variables.py
+++ ai4pdes/variables.py
@@ -1,9 +1,17 @@
+import torch
+
 def variable_shape(grid):
+    if grid.is2D():
+        shape = (1, 1, grid.ny , grid.nx )
+    else:
+        shape = (1, 1, grid.nz , grid.ny , grid.nx )
+    return shape
+
+def variable_shape_with_halo(grid):
     if grid.is2D():
         shape = (1, 1, grid.ny + 2*grid.halo, grid.nx + 2*grid.halo)
     else:
         shape = (1, 1, grid.nz + 2*grid.halo, grid.ny + 2*grid.halo, grid.nx + 2*grid.halo)
-    
     return shape
 
 class PrognosticVariables:
@@ -11,17 +19,25 @@ class PrognosticVariables:
         grid,
     ):
         shape = variable_shape(grid)
+        shape_with_halo = variable_shape_with_halo(grid)
         self.u = torch.zeros(shape, device=grid.device)
         self.v = torch.zeros(shape, device=grid.device)
         self.w = torch.zeros(shape, device=grid.device) if grid.is3D() else None
+
+        self.uu = torch.zeros(shape_with_halo, device=grid.device)
+        self.vv = torch.zeros(shape_with_halo, device=grid.device)
+        self.ww = torch.zeros(shape_with_halo, device=grid.device) if grid.is3D() else None
 
 class DiagnosticVariables:
     def __init__(self,
         grid,
     ):
         shape = variable_shape(grid)
+        shape_with_halo = variable_shape_with_halo(grid)
         self.p = torch.zeros(shape, device=grid.device)
         self.bu = torch.zeros(shape, device=grid.device)
         self.bv = torch.zeros(shape, device=grid.device)
         self.bw = torch.zeros(shape, device=grid.device) if grid.is3D() else None
+
+        self.pp = torch.zeros(shape_with_halo, device=grid.device)
 

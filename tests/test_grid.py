f62dd822c0363b2432d1ac2e2f630ea82b4dba85
pranavm19 <pranav.mamidanna@gmail.com>
2024-06-27 15:29:05 +0100

    Merging with changes to basic structure

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -2,7 +2,9 @@ import pytest
 import torch
 from ai4pdes.grid import Grid
 
-# Test default initialization
-
-
 # Test custom initialization
+def test_custom_2Dgrid():
+    grid = Grid(dx=0.2, dy=0.4, nx=32, ny=16, nz=1, halo=3, device='cpu')
+
+    assert grid.is2D == True
+    assert grid.lx == 

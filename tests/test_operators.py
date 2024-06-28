af78cec1eb5783b7930f17c9e438cd0eb437003e
pranavm19 <pranav.mamidanna@gmail.com>
2024-06-28 13:05:51 +0100

    updating operator tests

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -2,26 +2,14 @@ import pytest
 import numpy as np
 from ai4pdes.operators import get_weights_linear_2D
 
-# x-advection stencil/ filter with dx=0.5
-ADV_FILTER = np.array([[
-    [[ 0.1667],
-    [ 0.0000],
-    [-0.1667]],
-
-    [[ 0.6667],
-    [ 0.0000],
-    [-0.6667]],
-
-    [[ 0.1667],
-    [ 0.0000],
-    [-0.1667]]
-]])
-
 # Test x-advection (values)
 def test_xadv_operator():
     dx = 0.5
-    adv_operator = get_weights_linear_2D(dx)
-    assert np.allclose(adv_operator[1].numpy(), ADV_FILTER)
+    adv_operator = get_weights_linear_2D(dx)[2].numpy()[0, 0] # index 2 get x-adv
+
+    # Assert sum to zero per row
+    assert np.allclose(adv_operator.sum(axis=0), 0)
+    assert np.allclose(adv_operator.sum(), 0)
 
     # # test that this is close to what numpy.gradient does
     # xfield = np.random.randn(16, 16)
